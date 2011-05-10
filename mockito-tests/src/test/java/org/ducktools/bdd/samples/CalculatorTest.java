b4fb643fa4abde4903581dfb8ac6192eefd23e0d
Jan Ehrhardt <jan.ehrhardt@googlemail.com>
2011-05-10 22:04:00 +0200

    Mockito module added

---
+++ mockito-tests/src/test/java/org/ducktools/bdd/samples/CalculatorTest.java
@@ -0,0 +1,32 @@
+/**
+ * This is an example on using different frameworks for BDD on Java.
+ * Copyright (C) 2011 Jan Ehrhardt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.ducktools.bdd.samples;
+
+import org.junit.Test;
+
+/**
+ * @author <a href="https://github.com/derjan1982">Jan Ehrhardt</a>
+ */
+public class CalculatorTest {
+
+  @Test
+  public void itShouldCalculateThePrimeFactorsOfSix() {
+
+  }
+
+}

106d999bc90ce2cc58d9a0fa103caa57bd97bd06
Jan Ehrhardt <jan.ehrhardt@googlemail.com>
2011-05-11 13:27:53 +0200

    formatting fixed

--- cucumber-tests/features/step_definitions/PrimeFactorsSteps.groovy
+++ cucumber-tests/features/step_definitions/PrimeFactorsSteps.groovy
@@ -4,6 +4,10 @@ this.metaClass.mixin(cuke4duke.GroovyDsl)
 
 Given(~"a calculator") { -> calculator = new Calculator() }
 
-When(~"prime factors of (\\d+) are calculated") { int number -> result = calculator.primeFactors(number) }
+When(~"prime factors of (\\d+) are calculated") { int number ->
+  result = calculator.primeFactors(number)
+}
 
-Then(~"(.*) are the prime factors") { String expected -> assert expected == result.toString() }
+Then(~"(.*) are the prime factors") { String expected ->
+  assert expected == result.toString()
+}
